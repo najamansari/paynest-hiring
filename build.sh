cd0123fc2eafac3c64e1b0d0f27c4b86440aa8fd
Najam Ahmed Ansari <ahmed.najamansari@gmail.com>
2025-06-28 01:13:11 +0500

    Listing directory contents to debug

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 set -e
 
 echo "Initiating build process..."
-
+ls -alh
 # Build backend with serverless config
 cd bidder-backend
+ls -alh
 pnpm install --frozen-lockfile
 echo "Running BE serverless build..."
 pnpm run build:serverless
@@ -19,15 +20,16 @@ cp package.json ../backend/
 cd ../backend
 echo "Installing BE prod deps..."
 pnpm install --prod --frozen-lockfile
-
+ls -alh ../backend
 # Build frontend
 cd ../bidder-frontend
+ls -alh
 echo "Installing FE deps..."
 pnpm install --frozen-lockfile
 echo "Running FE build..."
 pnpm run build
 cp -R dist ../build
-
+ls -alh ../build
 # Cleanup
 rm -rf backend/package.json
 echo "Build completed successfully!"
